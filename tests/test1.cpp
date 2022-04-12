43a484663dbe66b97a56adc703981b017eb1afda
dmitry <paul.dm23@ya.ru>
2022-04-13 01:18:43 +0300

    unlinked tests

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,7 +2,39 @@
 #include <Transaction.h>
 #include <Account.h>
 
-TEST(Print, InFileStream)
-{
-  EXPECT_EQ(1, 1);
+TEST(Transaction, account) {
+    Account Acc(100, 100);
+    EXPECT_EQ(Acc.GetBalance(), 100);
+    EXPECT_EQ(Acc.id(), 100);
+}
+
+TEST(Transaction, change_balance) {
+    Account Acc(100, 100);
+    try {
+        Acc.ChangeBalance(100);
+    } catch (std::runtime_error& error){
+        EXPECT_EQ(error.what(), std::string("at first lock the account"));
+    }
+    Acc.Lock();
+    Acc.ChangeBalance(100);
+    EXPECT_EQ(Acc.GetBalance(), 200);
+}
+
+TEST(Transaction, transaction_sum) {
+    Account fAcc1(100, 110);
+    Account fAcc2(101, 101);
+    Account tAcc(200, 0);
+    Transaction transaction;
+    EXPECT_TRUE(transaction.Make(fAcc1, tAcc, 100));
+    EXPECT_FALSE(transaction.Make(fAcc2, tAcc, 100));
+}
+
+TEST(Transaction, transaction_id) {
+    Account Acc(100, 110);
+    Transaction transaction;
+    try {
+        EXPECT_FALSE(transaction.Make(Acc, Acc, 100));
+    } catch (std::logic_error& error){
+        EXPECT_EQ(error.what(), std::string("invalid action"));
+    }
 }
